c5879f4f971e72196e02b0e4241b05caec250b8f
colenocks <enockscoleman@gmail.com>
2020-02-01 03:47:45 +0000

    Power gauge bar function

--- js/app.js
+++ js/app.js
@@ -20,4 +20,26 @@ function loading() {
   }, 3000);
 }
 
+let button = document.querySelectorAll("button");
+for (let btn = 0; btn < button.length; btn++) {
+  button[btn].onclick = function() {
+    button[btn].style.transform = "scale(1.2)";
+    setTimeout(() => {
+      button[btn].style.transform = "scale(1)";
+    }, 100);
+  };
+}
+
+/* Gauging bar */
+let guageBtn = document.querySelector(".gauge");
+let bar = document.querySelector(".shoot-bar");
+let barVal = bar.getAttribute("value");
+let barMax = bar.getAttribute("max");
+let count = 0;
+guageBtn.onclick = function() {
+  if (barVal != barMax) {
+    count++;
+    bar.setAttribute("value", count);
+  }
+};
 // alert(window.innerHeight);
