b70eb0a8dfb80f3c6039b103fdb9a7183933bcab
colenocks <enockscoleman@gmail.com>
2020-02-10 18:02:18 +0000

    assigned global values/constants to a class

---
+++ js/global.js
@@ -0,0 +1,22 @@
+/* -----------GLOBAL SETTINGS------------- */
+const canvasWidth = 580;
+const canvasHeight = 400;
+const gravity = 1;
+const friction = 0.99;
+const startingPosx = 36; //ball position x
+const startingPosy = 280; //ball position y
+const ballRadius = 14;
+const ballColor = "#ff9d00";
+const ballCOR = -0.89; //coefficient of restitution
+
+export {
+  canvasHeight,
+  canvasWidth,
+  gravity,
+  friction,
+  startingPosx,
+  startingPosy,
+  ballColor,
+  ballCOR,
+  ballRadius
+};
